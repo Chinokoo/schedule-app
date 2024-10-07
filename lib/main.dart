5a5aa87336c76d6d8981119d915000c9f3de4ca4
Peter <peterchinokoo@gmail.com>
2024-10-07 18:23:26 +0200

    Create: Intro Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:rosemary_app/pages/navigation.dart';
+import 'package:rosemary_app/pages/intro_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: NavigatePages(),
+      home: IntroPage(),
     );
   }
 }
---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:rosemary_app/pages/navigation.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff85A389),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Productivity Plus",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 50,
+                  fontStyle: FontStyle.italic),
+            ),
+            GestureDetector(
+              onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const NavigatePages())),
+              child: Container(
+                  padding: const EdgeInsets.all(20),
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white, width: 2),
+                      borderRadius: BorderRadius.circular(30)),
+                  child: const Center(
+                      child: Text(
+                    'Get Started',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ))),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
